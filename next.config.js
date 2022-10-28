ce10a1fc073555c156a6bb0d234c6858e3b620fb
Oluwafemi Kayode <mrlapinho@gmail.com>
2022-10-28 16:23:16 +0100

    Project config and install new npm packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
+  trailingSlash: false,
   env: {
     SITE_URL: "site url",
     SITE_TITLE: "Market Design",
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,37 @@ module.exports = {
         "3xl": "1920px",
       },
       fontFamily: {
-        sans: ["Raleway", ...defaultTheme.fontFamily.sans],
+        sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
+        poppins: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "light-grey": "#BBBBBB",
+        "dark-grey": "#7E7E7E",
+        green: {
+          dark: "#7DBB67",
+          light: "#B7CDAF",
+        },
+        red: "#DE2B2B",
+        brown: "#9D7F69",
+        blue: {
+          light: "#80CCE4",
+        },
+      },
+      keyframes: {
+        open: {
+          from: { height: "0" },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        close: {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        open: "open .3s ease-in-out",
+        close: "close .4s ease-in-out",
       },
     },
   },
