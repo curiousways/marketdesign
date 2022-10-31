bc9f767b8418ad8ec054bec4e0052b569571f7fa
David Flindall <david@curiousways.com>
2022-10-31 12:34:23 +0000

    align nav items end, adjust nav padding

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -5,7 +5,7 @@ import Logo from "./Logo";
 
 const Nav = () => {
   return (
-    <div className="px-10 py-5 lg:flex justify-between items-center bg-white">
+    <div className="px-10 pb-8 pt-5 lg:flex justify-between items-end bg-white">
       <Link href="/" passHref>
         <a aria-label="Exeter Lindsay">
           <Logo />
