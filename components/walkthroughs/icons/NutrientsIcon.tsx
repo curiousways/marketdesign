22a8e51fd3bb6842e041c0a1dc04070c25b2eb00
Oluwafemi Kayode <mrlapinho@gmail.com>
2022-11-14 08:27:33 +0000

    Add SVG icons as components

---
+++ components/walkthroughs/icons/NutrientsIcon.tsx
@@ -0,0 +1,147 @@
+import React from "react";
+
+const NutrientsIcon = () => {
+  return (
+    <svg
+      width="23"
+      height="30"
+      viewBox="0 0 23 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g opacity="0.7" filter="url(#filter0_ddiiii_1249_24260)">
+        <path
+          d="M11.5 27C9.24575 27 7.08361 26.2283 5.48959 24.8545C3.89553 23.4807 3 21.6176 3 19.6749C3 16.666 4.61495 14.4108 6.58258 11.6631C8.11304 9.52596 9.85682 7.09082 11.2202 3.77137C11.2998 3.57737 11.4987 3 11.4987 3C11.4987 3 11.4992 3.00126 11.5 3.00366C11.5008 3.00126 11.5013 3 11.5013 3C11.5013 3 11.7002 3.57737 11.7798 3.77137C13.1432 7.09082 14.887 9.52596 16.4174 11.6631C18.385 14.4108 20 16.666 20 19.6749C20 21.6176 19.1045 23.4807 17.5104 24.8545C15.9276 26.2185 13.7385 27 11.5 27Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_ddiiii_1249_24260"
+          x="-2"
+          y="-2"
+          width="27"
+          height="34"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="-1" dy="-1" />
+          <feGaussianBlur stdDeviation="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.788235 0 0 0 0 0.788235 0 0 0 0 0.788235 0 0 0 0.5 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_1249_24260"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="1" dy="1" />
+          <feGaussianBlur stdDeviation="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.3 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect1_dropShadow_1249_24260"
+            result="effect2_dropShadow_1249_24260"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect2_dropShadow_1249_24260"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="5" dy="5" />
+          <feGaussianBlur stdDeviation="6.5" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.788235 0 0 0 0 0.788235 0 0 0 0 0.788235 0 0 0 0.9 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect3_innerShadow_1249_24260"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="-5" dy="-5" />
+          <feGaussianBlur stdDeviation="5" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.9 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect3_innerShadow_1249_24260"
+            result="effect4_innerShadow_1249_24260"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="5" dy="-5" />
+          <feGaussianBlur stdDeviation="5" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.788235 0 0 0 0 0.788235 0 0 0 0 0.788235 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect4_innerShadow_1249_24260"
+            result="effect5_innerShadow_1249_24260"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="-5" dy="5" />
+          <feGaussianBlur stdDeviation="5" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.788235 0 0 0 0 0.788235 0 0 0 0 0.788235 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="effect5_innerShadow_1249_24260"
+            result="effect6_innerShadow_1249_24260"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default NutrientsIcon;
