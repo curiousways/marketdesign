ea48725ed5278da22df15bfd4c92c946f16058c0
Alex Mendes <alexanderhmendes@gmail.com>
2022-11-14 14:23:02 +0000

    feat: add header to the how it works index page

--- pages/how-it-works/index.tsx
+++ pages/how-it-works/index.tsx
@@ -1,32 +1,43 @@
 import type { NextPage } from "next";
 import { walkthroughs } from "data/walkthroughs";
 import Link from "next/link";
+import Header from "@/components/common/Header";
+import HeaderImg from "../../public/assets/images/how-it-works/header.png";
+import HeaderThumb from "../../public/assets/images/how-it-works/header-thumb.png";
 
 const HowItWorks: NextPage = () => (
-  <ul>
-    {walkthroughs.map((walkthrough) => (
-      <li key={walkthrough.title}>
-        <h2>Walkthrough {walkthrough.id}</h2>
-        <p>{walkthrough.title}</p>
-        <ul>
-          {walkthrough.scenarios.map((scenario) => (
-            <li key={scenario.title}>
-              <Link
-                href={{
-                  pathname: '/how-it-works/[scenarioId]',
-                  query: {
-                    scenarioId: scenario.id,
-                  },
-                }}
-              >
-                <a>{scenario.id} {scenario.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </li>
-    ))}
-  </ul>
+  <>
+    <Header
+      title="How it works"
+      description="A series of walkthroughs designed to introduce you to the Exeter-Lindsay model and all of it's features."
+      mainImageSrc={HeaderImg}
+      secondaryImageSrc={HeaderThumb}
+    />
+    <ul>
+      {walkthroughs.map((walkthrough) => (
+        <li key={walkthrough.title}>
+          <h2>Walkthrough {walkthrough.id}</h2>
+          <p>{walkthrough.title}</p>
+          <ul>
+            {walkthrough.scenarios.map((scenario) => (
+              <li key={scenario.title}>
+                <Link
+                  href={{
+                    pathname: '/how-it-works/[scenarioId]',
+                    query: {
+                      scenarioId: scenario.id,
+                    },
+                  }}
+                >
+                  <a>{scenario.id} {scenario.title}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+  </>
 );
 
 export default HowItWorks;
