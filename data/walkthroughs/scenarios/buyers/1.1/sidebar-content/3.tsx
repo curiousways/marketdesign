5cb8b0b1ce808c0a9aca9e3efacd97063c168f82
Alex Mendes <alexanderhmendes@gmail.com>
2022-11-14 22:50:05 +0000

    feat: update buyer walkthrough 1.1

---
+++ data/walkthroughs/scenarios/buyers/1.1/sidebar-content/3.tsx
@@ -0,0 +1,16 @@
+export const sidebarContent3 = (
+  <>
+    <p>
+      You decide to buy the credits you need in the credit market.
+    </p>
+    <p>
+      In this walkthrough there happen to be 3 credit sellers and 2 other buyers
+      in that market.
+    </p>
+    <p>
+      While you wouldn't know this information in a real market, here we have
+      shown you full details of the offers and bids made by those other market
+      participants.
+    </p>
+  </>
+);
---
+++ data/walkthroughs/scenarios/buyers/1.1/sidebar-content/6.tsx
@@ -0,0 +1,10 @@
+export const sidebarContent6 = (
+  <>
+    <p>
+      The market is now closed for bids and offers.
+    </p>
+    <p>
+      Press the solve market button to see how you get on when bidding at value.
+    </p>
+  </>
+);
