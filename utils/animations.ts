0e165ac6ab95a1ba91b5fc625b51c7d36c7b42da
Oluwafemi Kayode <mrlapinho@gmail.com>
2022-11-14 07:33:21 +0000

    Adjyst styles and add framer motion variants utils

--- utils/animations.ts
+++ utils/animations.ts
@@ -9,13 +9,34 @@ export const container = {
   },
 };
 
-export const item = {
-  hidden: { opacity: 0, y: -10 },
+export const fadeInDown = {
+  hidden: {
+    opacity: 0,
+    y: -10,
+    transition: {
+      duration: 0.3,
+    },
+  },
   visible: {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 0.5,
+      duration: 0.3,
+    },
+  },
+};
+
+export const fadeIn = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+    },
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.3,
     },
   },
 };
