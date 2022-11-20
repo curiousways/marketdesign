54469b76f9eb84ba73bd2a2a9d93c519e29ed38d
Alex Mendes <alexanderhmendes@gmail.com>
2022-11-20 17:25:20 +0000

    style: tweak linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -126,6 +126,9 @@ module.exports = {
         '@typescript-eslint/unified-signatures': 'error',
         '@typescript-eslint/naming-convention': 'off',
         '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-misused-promises': [
           'error',
           { checksVoidReturn: false },
