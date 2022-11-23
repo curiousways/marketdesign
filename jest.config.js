4c4bb2b88943d77e23b397ae77b194747f671dae
Alex Mendes <alexanderhmendes@gmail.com>
2022-11-23 22:12:19 +0000

    refctor: make the LoadingOverlay more reusable

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,13 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
   },
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/lib/(.*)$': '<rootDir>/lib/$1',
+    '^@/hooks/(.*)$': '<rootDir>/hooks/$1',
+    '^@/utils/(.*)$': '<rootDir>/utils/$1',
+    '^@/styles/(.*)$': '<rootDir>/styles/$1',
+    '^@/context/(.*)$': '<rootDir>/context/$1',
+    '^@/types/(.*)$': '<rootDir>/types/$1',
+  },
 };
