b294c4b666e77eed246c20416ecae2e2a79ebc3b
Alex Mendes <alexanderhmendes@gmail.com>
2022-11-24 18:44:39 +0000

    feat: pull out Pagination component

---
+++ components/common/DescriptionBox/index.tsx
@@ -0,0 +1,28 @@
+import React, { FC, ReactNode } from 'react';
+import { motion } from 'framer-motion';
+import { fadeIn } from '@/utils/animations';
+
+type DescriptionBoxProps = {
+  children?: ReactNode;
+};
+
+export const DescriptionBox: FC<DescriptionBoxProps> = ({
+  children,
+}: DescriptionBoxProps) => {
+  const hide = !children;
+
+  return (
+    <motion.div
+      data-testid="description-box"
+      variants={fadeIn}
+      initial="hidden"
+      animate={hide ? 'hidden' : 'visible'}
+      layout
+      className="text-l"
+    >
+      <div className="bg-green-dark py-5 px-3 rounded-lg text-white">
+        <div className="space-y-5 text-white">{children}</div>
+      </div>
+    </motion.div>
+  );
+};
