d6a729064c7b82e0d9714ffe0d18eb1b7775474a
Alex Mendes <alexanderhmendes@gmail.com>
2022-11-25 20:00:27 +0000

    fix: more wording tweaks

--- data/walkthroughs/scenarios/sellers/5.1/sidebar-content/2.tsx
+++ data/walkthroughs/scenarios/sellers/5.1/sidebar-content/2.tsx
@@ -5,7 +5,7 @@ export const sidebarContentStage2 = (
       credit at a cost of £140,000.
     </p>
     <p>
-      While the project on Field 2 delivers 1 biodiversity credits and 2 water
+      While the project on Field 2 delivers 1 biodiversity credit and 2 water
       quality credits at a cost of £70,000.
     </p>
   </>
