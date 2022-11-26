983b20e4f0acbc5043ebdfd804efa679d0fcf346
Alex Mendes <alexanderhmendes@gmail.com>
2022-11-26 13:58:18 +0000

    test: add some tests

--- components/common/ProductCount/index.tsx
+++ components/common/ProductCount/index.tsx
@@ -11,6 +11,7 @@ export const ProductCount: FC<ProductCountProps> = ({
   className,
 }: ProductCountProps) => (
   <div
+    data-testid="product-count"
     className={classNames(
       'absolute right-0 border-black rounded-full bg-white w-[29px] h-[29px] flex justify-center items-center',
       className,
