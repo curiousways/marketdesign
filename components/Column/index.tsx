3484caef6f564c020a897c0248442b65e6c0dc3f
Oluwafemi Kayode <mrlapinho@gmail.com>
2022-12-05 12:32:03 +0100

    Add InlineTitle component with bg presense prop

--- components/Column/index.tsx
+++ components/Column/index.tsx
@@ -3,12 +3,14 @@ import { ReactNode } from 'react';
 import { classNames } from '@/utils/index';
 
 import { Button } from '@/components/Button';
+import { InlineTitle } from "@/components/InlineTitle/index"
 
 type Props = {
   title?: string;
   button?: boolean;
   buttonLink?: string;
   buttonText?: string;
+  titlebackground?: boolean;
   titleClass?: string;
   className?: string;
   children: ReactNode;
@@ -20,12 +22,17 @@ export const Column = ({
   buttonLink,
   buttonText,
   className,
-  titleClass,
   children,
+  titlebackground,
+  titleClass,
 }: Props) => {
   return (
     <div className={classNames('max-w-[616px] space-y-10', className)}>
-      <h2 className={classNames('heading-2', titleClass)}>{title && title}</h2>
+      <InlineTitle
+        titleClass={titleClass ?? ""}
+        title={title ?? ''}
+        background={titlebackground}
+      />
       {children}
 
       {button && <Button text={buttonText ?? ''} link={buttonLink ?? ''} />}
---
+++ components/InlineTitle/index.tsx
@@ -0,0 +1,34 @@
+import { classNames } from '@/utils/index';
+
+type Props = {
+  title: string;
+  background?: boolean;
+  titleClass?: string;
+};
+
+export const InlineTitle = ({
+  title,
+  titleClass,
+  background = false,
+}: Props) => {
+  return (
+    <div className="relative">
+      {background && (
+        <svg
+          width="753"
+          height="317"
+          viewBox="0 0 753 317"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute -top-24 -left-6 -z-[1] max-w-[783px]"
+        >
+          <path
+            d="M0 96.6406L64.5562 0L571.45 38.0493L753 228.063L636.473 317L33.0938 264.945L0 96.6406Z"
+            fill="#FFFCEF"
+          />
+        </svg>
+      )}
+      <h2 className={classNames('heading-2', titleClass ? titleClass : "")}>{title && title}</h2>
+    </div>
+  );
+};
