211a3be68a1bb50694991b9f816fb6bbb9091734
Oluwafemi Kayode <mrlapinho@gmail.com>
2022-12-05 12:02:06 +0100

    add prop for switching left image / text position

--- components/ImageTextGrid/index.tsx
+++ components/ImageTextGrid/index.tsx
@@ -1,16 +1,22 @@
 import { ReactNode } from 'react';
 import Image, { StaticImageData } from 'next/image';
 
+import { classNames } from '@/utils/index';
+
 type Props = {
   img: StaticImageData;
   alt?: string;
   text_title: string;
+  reverse?: boolean
   children: ReactNode;
 };
 
-export const ImageTextGrid = ({ img, alt, text_title, children }: Props) => {
+export const ImageTextGrid = ({ img, alt, text_title, reverse = false, children }: Props) => {
   return (
-    <div className="lg:flex lg:gap-x-10 xl:gap-x-20 xl:space-y-16">
+    <div
+      className={classNames('lg:flex lg:gap-x-10 xl:gap-x-20 xl:space-y-16',
+      reverse ?'lg:flex-row-reverse items-center': 'lg:flex-row')}
+    >
       <div className="max-w-[833px] relative z-10">
         <Image
           src={img}
