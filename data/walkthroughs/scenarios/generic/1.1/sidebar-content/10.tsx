b58985882e7007e7b4aa5bc2dfe279bd2ef9ea8e
Alex Mendes <alexanderhmendes@gmail.com>
2022-12-11 14:33:50 +0000

    fix: more walkthrough 0 fixes

--- data/walkthroughs/scenarios/generic/1.1/sidebar-content/10.tsx
+++ data/walkthroughs/scenarios/generic/1.1/sidebar-content/10.tsx
@@ -1,10 +1,10 @@
 export const sidebarContent10 = (
   <>
     <p>
-      In the market solution shown here, seller 1 and buyer 2 are selected as
+      In the market solution shown here, buyer 2 and seller 2 are selected as
       winners. We can see this is feasible because the credits quantities
-      provided by seller 1 are greater than the quantities demanded by buyer 2.
-      Moreover, buyer 2’s bid is greater than the payment request from seller 1.
+      provided by seller 2 are greater than the quantities demanded by buyer 2.
+      Moreover, buyer 2’s bid is greater than the payment request from seller 2.
       The difference between how much buyers are prepared to pay and what
       sellers requested as payments is known as the Market Surplus. In this
       case, that is £20,000.
