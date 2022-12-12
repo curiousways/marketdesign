bfd1578c559357b531dcedbdf2359055b79e5fff
Alex Mendes <alexanderhmendes@gmail.com>
2022-12-12 13:05:46 +0000

    fix: dividing line for shared bids

--- components/MarketParticipantList/index.tsx
+++ components/MarketParticipantList/index.tsx
@@ -108,6 +108,7 @@ export const MarketParticipantList: FC<MarketParticipantListProps> = ({
               isMyProject={myProjects.includes(project)}
               isGroupedProject={isGroupedProject}
               isDivisible={isDivisible}
+              hasSharedCost={!!project.sharedCost}
               isFirstGroupedProject={isFirstGroupedProject}
               isLastGroupedProject={isLastGroupedProject}
               isSubsequentGroupedProject={
